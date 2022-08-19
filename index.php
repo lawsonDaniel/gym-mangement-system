3a675dbae791d7f4afdda0fb1fb2b898e7f396f2
Lawson Daniel <lawblaze4@gmail.com>
2022-08-19 13:28:32 +0200

    done with contact

--- index.php
+++ index.php
@@ -1,52 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-  </head>
-  <style type="text/css">
-    #inputbtn:hover{cursor:pointer;}
-  </style>
-  <body style="background:url('images/4.jpg'); background-size: cover;">
-    <div class="container-fluid" style="margin-top:60px;margin-bottom:60px;color:#34495E;">
-      <div class="row">
-        <div class="col-md-1"></div>
-        <div class="col-md-4">
-          <div class="card">
-            <img src="images/cardback.jpg" class="card-img-top">
-            <div class="card-body">
-              <center>
-              <h5>Admin Login</h5><br>
-              <form class="form-group" method="POST" action="admin-panel.php">
-                <div class="row">
-                  <div class="col-md-4"><label>Username: </label></div>
-                  <div class="col-md-8"><input type="text" name="username" class="form-control" placeholder="enter username" required/></div><br><br>
-                  <div class="col-md-4"><label>Password: </label></div>
-                  <div class="col-md-8"><input type="password" class="form-control" name="password" placeholder="enter password" required/></div><br><br><br>
-                </div>
-                <center><input type="submit" id="inputbtn" name="login_submit" value="Login" class="btn btn-primary"></center>
-              </form>
-            
-              
-            </center>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Silkscreen:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+<link href="https://unpkg.com/pattern.css" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <link rel="stylesheet" href="./Asset/Style.css">
+</head>
+<body>
+  <header>
+      <div class="logo">LOGO</div>
+      <nav>
+          <ul>
+              <li><a class="active" href="#">Home</a></li>
+              <li class=""><a href="#about">About</a></li>
+              <li class=""><a href="#contact">Contact</a></li>
+          </ul>
+          <ul>
+              <li><a class="active" href="./login.php">Login</a></li>
+              <li><a class="button" href="./signup.php">Sign Up</a></li>
+          </ul>
+      </nav>
+  </header>
+  <main>
+      <section id="jumbotron">
+          <div class="jumbotron-header">
+           <h1> <span>Transform</span> <br/> Your Body</h1>
+           <div class="jumbotron-p">We help you to create the shape of your <br/> body and stay healthy</div>
+           <a class="jumbotron-button" href="./signup.php">Sign Up</a>
+          </div>
+          <div class="jumbotron-img ">
+          <img src="./images/sit.jpg"/>
+          </div>
+      </section>
+      <section class="about" id="about">
+          <div class="about-header">About Us</div>
+          <div class="about-content">
+            <div class="about-img pattern-diagonal-lines-sm gray-lighter"></div>
+            <div class="about-content-container">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas consequuntur temporibus eius
+               dolore sunt ad obcaecati quaerat non ex nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque dolorem nihil officia libero, cupiditate ipsam, illo quaerat ullam voluptates laborum ipsa pariatur, officiis natus? Eaque sit et dolores cum laboriosam doloribus nihil, cumque,
+                officiis harum corporis explicabo eum, aut tenetur!</p>
+               <div class="about-btn-container ">
+            <a href="#" class="about-btn ">Sign Up</a>
+            <span class="material-symbols-outlined">
+              chevron_right
+              </span>
+          </div>
+            </div>
             </div>
           </div>
-        </div>
-         <div class="col-md-7"></div>
+      </section>
+      <section id="contact">
+      <h1 class="section-header">Contact</h1>
+      <div class="contact-wrapper">
+      
+      <!-- Left contact page --> 
+        
+        <form id="contact-form" class="form-horizontal" role="form">
+          
+          <div class="form-group">
+            <div class="col-sm-12">
+              <input type="text" class="form-control" id="name" placeholder="NAME" name="name" value="" required>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="col-sm-12">
+              <input type="email" class="form-control" id="email" placeholder="EMAIL" name="email" value="" required>
+            </div>
+          </div>
+
+          <textarea class="form-control" rows="10" placeholder="MESSAGE" name="message" required></textarea>
+          
+          <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">
+            <div class="alt-send-button">
+              <i class="fa fa-paper-plane"></i><span class="send-text">SEND</span>
+            </div>
+          
+          </button>
+          
+        </form>
+        
+      <!-- Left contact page --> 
+        
+        
+        
       </div>
-    </div>
-
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-  </body>
+  
+</section>  
+  
+  
+  </main>
+</body>
 </html>
